210deabc68279a51d960b4349c603703cae9d014
Chris Howard <47666748+C-Howard@users.noreply.github.com>
2024-09-09 16:46:53 -0400

    Update setup-terraform dependencies, Rebuild action with updated dependencies (#97)
    * Remove typescript dependency
    
    * Fix typescript error type checks
    
    * Replace zeit/ncc with vercel/ncc
    
    * Update package dependencies
    
    * Rebuild setup-terraform with updated dependencies

--- setup-terraform/src/index.ts
+++ setup-terraform/src/index.ts
@@ -6,7 +6,9 @@ async function run() {
     const version = core.getInput("version");
     await installer.getTerraform(version);
   } catch (error) {
-    core.setFailed(error.message);
+    if (error instanceof Error) {
+      core.setFailed(error.message);
+    }
   }
 }
 
